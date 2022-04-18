aa54567c8120ad648750a6a751e3ae747d297be9
Qiheng Gao <qiheng.gao@duke.edu>
2022-04-17 21:00:58 -0400

    in work

--- miniUPS/UPS.py
+++ miniUPS/UPS.py
@@ -1,9 +1,11 @@
 from google.protobuf.internal.decoder import _DecodeVarint32
 from google.protobuf.internal.encoder import _EncodeVarint
-
+from protos import world_ups_pb2 as World_UPS
+from protos import UA_pb2 as UA
 
 class UPS:
-    def __init__(self, to_world_socket=None, to_amazon_socket=None):
+    def __init__(self, world_id = -1, to_world_socket=None, to_amazon_socket=None):
+        self.world_id = world_id
         self.to_world_socket = to_world_socket
         self.to_amazom_socket = to_amazon_socket
 
@@ -13,16 +15,50 @@ class UPS:
     def set_amazon_socket(self, to_amazon_socket):
         self.to_amazom_socket = to_amazon_socket
 
-    def write_msg(self, s, msg):
+    # Helper function for writing message
+    def write_msg(self, socket_, msg):
         string_msg = msg.SerializeToString()
-        _EncodeVarint(s.send, len(string_msg), None)
-        s.send(string_msg)
+        _EncodeVarint(socket_.send, len(string_msg), None)
+        socket_.send(string_msg)
+
+    # Helper function for receiving message
+    def recv_msg(self, socket_) -> str:
+        var_int_buff = []
+        while True:
+            buf = socket_.recv(1)
+            var_int_buff += buf
+            msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+            if new_pos != 0:
+                break
+        whole_message = socket_.recv(msg_len)
+        return whole_message
 
     def write_to_world(self, msg):
         self.write_msg(self.to_world_socket, msg)
-    
-    def write_to_amz(self, msg):
+
+    def write_to_amz(self, msg: UA.UAmessage):
         self.write_msg(self.to_amazom_socket, msg)
 
+    # Recv from world's response after successfully connected
     def recv_from_world(self):
-        return
+        whole_message = self.recv_msg(self.to_world_socket)
+        world_res = World_UPS.UResponses()
+        world_res.ParseFromString(whole_message)
+        return world_res
+
+    def recv_from_amz(self) -> UA.AUmessage:
+        whole_message = self.recv_msg(self.to_amazom_socket)
+        au_msg = UA.AUmessage()
+        au_msg.ParseFromString(whole_message)
+        return au_msg
+    
+    def reconnect_to_word(self) -> bool:
+        msg = World_UPS.UConnect()
+        msg.worldid = self.world_id
+        msg.isAmazon = False
+        self.write_to_world(msg)
+        uconnected = World_UPS.UConnected()
+        uconnected.ParseFromString(self.recv_msg(self.to_world_socket))
+        if uconnected.result == "connected!":
+            return True
+        return False
--- miniUPS/to_world.py
+++ miniUPS/to_world.py
@@ -1,13 +1,35 @@
 import socket
+import UPS
 from protos import world_ups_pb2 as World_UPS
 
+def get_socket_to_amz():
+    ups_host = '127.0.0.1'
+    ip_port_amz = (ups_host, 8888)
+    listen_to_amz = socket.socket()
+    listen_to_amz.bind(ip_port_amz)
+    listen_to_amz.listen(5)
+    socket_to_amz, address = listen_to_amz.accept()
+    return socket_to_amz
+
+def get_socket_to_world():
+    world_host = 'vcm-26338.vm.duke.edu'
+    ip_port_w = (world_host, 12345)
+    socket_to_world = socket.socket()
+    socket_to_world.connect(ip_port_w)
+    return socket_to_world
+
+def get_world_id_from_input():
+    # ask user to reconnect or create a new world
+    return None
 
 def main():
-    world_host = 'vcm-26338.vm.duke.edu'
-    ip_port = (world_host, 12345)
-    socket_to_world = socket.socket()
-    socket_to_world.connect(ip_port)
-    world_id = None
+    world_id = get_world_id_from_input()
+    socket_to_world = get_socket_to_world()
+    socket_to_amz = get_socket_to_amz()
+
+    ups = UPS(world_id, socket_to_world, socket_to_amz)
+
+    ups.reconnect_to_world()
     
     # create new world or receonnect
     if world_id:
@@ -15,7 +37,9 @@ def main():
         # get the host name of world from user input (default localhost)
 
         # set connect info
-
+    # while True:
+    # thread ? ups.handleW()
+    # thread ? ups.hadnleA() share the same ups object
 
 if __name__ == "__main__":
     main()
