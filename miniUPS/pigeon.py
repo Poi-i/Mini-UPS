8d2c9cef8ea55c94d10f0ede94664c0d40c53f4b
bw224 <bw224@duke.edu>
2022-04-17 22:14:46 -0400

    pigeon

---
+++ miniUPS/pigeon.py
@@ -0,0 +1,75 @@
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.internal.encoder import _EncodeVarint
+from protos import world_ups_pb2 as World_UPS
+from protos import UA_pb2 as UA
+
+# communication tool to Amazon and World
+
+# def __init__(self, world_id=-1, to_world_socket=None, to_amazon_socket=None):
+#     self.world_id = world_id
+#     self.to_world_socket = to_world_socket
+#     self.to_amazom_socket = to_amazon_socket
+
+# def set_world_socket(self, to_world_socket):
+#     self.to_world_socket = to_world_socket
+
+# def set_amazon_socket(self, to_amazon_socket):
+#     self.to_amazom_socket = to_amazon_socket
+
+# Helper function for writing, write msg to socket_
+
+
+def write_msg(socket_, msg):
+    string_msg = msg.SerializeToString()
+    _EncodeVarint(socket_.send, len(string_msg), None)
+    socket_.send(string_msg)
+
+# Helper function for receiving, recv from socket_
+
+
+def recv_msg(socket_) -> str:
+    var_int_buff = []
+    while True:
+        buf = socket_.recv(1)
+        var_int_buff += buf
+        msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+        if new_pos != 0:
+            break
+    whole_message = socket_.recv(msg_len)
+    return whole_message
+
+
+def write_to_world(to_world_socket, msg):
+    write_msg(to_world_socket, msg)
+
+
+def write_to_amz(to_amazom_socket, msg: UA.UAmessage):
+    write_msg(to_amazom_socket, msg)
+
+# Recv from world's response after successfully connected
+
+
+def recv_from_world(to_world_socket):
+    whole_message = recv_msg(to_world_socket)
+    world_res = World_UPS.UResponses()
+    world_res.ParseFromString(whole_message)
+    return world_res
+
+
+def recv_from_amz(to_amazom_socket) -> UA.AUmessage:
+    whole_message = recv_msg(to_amazom_socket)
+    au_msg = UA.AUmessage()
+    au_msg.ParseFromString(whole_message)
+    return au_msg
+
+
+def reconnect_to_word(world_id, to_world_socket) -> bool:
+    msg = World_UPS.UConnect()
+    msg.worldid = world_id
+    msg.isAmazon = False
+    write_to_world(msg)
+    uconnected = World_UPS.UConnected()
+    uconnected.ParseFromString(recv_msg(to_world_socket))
+    if uconnected.result == "connected!":
+        return True
+    return False
--- miniUPS/to_world.py
+++ miniUPS/to_world.py
@@ -1,6 +1,7 @@
 import socket
-import UPS
+import pigeon
 from protos import world_ups_pb2 as World_UPS
+
 
 def get_socket_to_amz():
     ups_host = '127.0.0.1'
@@ -11,6 +12,7 @@ def get_socket_to_amz():
     socket_to_amz, address = listen_to_amz.accept()
     return socket_to_amz
 
+
 def get_socket_to_world():
     world_host = 'vcm-26338.vm.duke.edu'
     ip_port_w = (world_host, 12345)
@@ -18,28 +20,27 @@ def get_socket_to_world():
     socket_to_world.connect(ip_port_w)
     return socket_to_world
 
+
 def get_world_id_from_input():
     # ask user to reconnect or create a new world
     return None
+
 
 def main():
     world_id = get_world_id_from_input()
     socket_to_world = get_socket_to_world()
     socket_to_amz = get_socket_to_amz()
 
-    ups = UPS(world_id, socket_to_world, socket_to_amz)
+    # send connect/reconnect to world
 
-    ups.reconnect_to_world()
-    
-    # create new world or receonnect
-    if world_id:
-        return
-        # get the host name of world from user input (default localhost)
+    # send the world_id to amz
 
-        # set connect info
-    # while True:
-    # thread ? ups.handleW()
-    # thread ? ups.hadnleA() share the same ups object
+    # start one process to dock world
+    p_to_world = Process(target=dock_world, args=(
+        socket_to_world, socket_to_amz))
+    # start one process to dock amz
+    p_to_amz = Process(target=dock_amz, args=(socket_to_world, socket_to_amz))
+
 
 if __name__ == "__main__":
     main()
