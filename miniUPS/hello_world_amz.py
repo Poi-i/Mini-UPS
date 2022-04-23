d803ea2ea2051adbac98f2b7cf693680db1bfa59
Qiheng Gao <qiheng.gao@duke.edu>
2022-04-23 16:27:07 -0400

    daemon thread for test

--- miniUPS/hello_world_amz.py
+++ miniUPS/hello_world_amz.py
@@ -3,21 +3,22 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miniUPS.settings")
 if django.VERSION >= (1, 7):
     django.setup()
-import sys
-from concurrent.futures import ThreadPoolExecutor
-import socket
-from threading import Thread
-import threading
-import time
-from google.protobuf.internal.decoder import _DecodeVarint32
-from google.protobuf.internal.encoder import _EncodeVarint
-from protos import world_ups_pb2 as World_UPS
-from protos import UA_pb2 as UA
-import PBwrapper
-from django.db.models import Q
-import website.models as md
 
-# import pigeon
+import website.models as md
+from django.db.models import Q
+import PBwrapper
+from protos import UA_pb2 as UA
+from protos import world_ups_pb2 as World_UPS
+from google.protobuf.internal.encoder import _EncodeVarint
+from google.protobuf.internal.decoder import _DecodeVarint32
+import time
+import threading
+from threading import Thread
+import socket
+from concurrent.futures import ThreadPoolExecutor
+import sys
+
+
 
 
 executer = ThreadPoolExecutor(40)
@@ -35,6 +36,7 @@ def get_socket_to_amz():
     ip_port_amz = (ups_host, 54321)
     listen_to_amz = socket.socket()
     try:
+        listen_to_amz.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listen_to_amz.bind(ip_port_amz)
         listen_to_amz.listen(5)
         socket_to_amz, address = listen_to_amz.accept()
@@ -51,6 +53,7 @@ def get_socket_to_world():
     ip_port_w = (world_host, 12345)
     socket_to_world = socket.socket()
     try:
+        socket_to_world.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
         socket_to_world.connect(ip_port_w)
         print("connect to world" + "\n")
         return socket_to_world
@@ -58,7 +61,6 @@ def get_socket_to_world():
         print("having problem in connectiong to world\n")
         socket_to_world.close()
         sys.exit(1)
-        
 
 
 def get_seqnum() -> int:
@@ -69,17 +71,11 @@ def get_seqnum() -> int:
     lock.release()
     return to_ret
 
-
-def get_world_id_from_input():
-    # ask user to reconnect or create a new world
-    return None
-
-
 def dock_amz(socket_to_world, socket_to_amz):
     print("dock to amz")
     count = 0
     while True:
-        print("recv from amz: " + str(count) + "\n")
+        print("recv from amz: " + str(count))
         count += 1
         au_msg, is_socket_closed = recv_from_amz(socket_to_amz)
         if is_socket_closed:
@@ -89,7 +85,6 @@ def dock_amz(socket_to_world, socket_to_amz):
         executer.submit(handle_amz, au_msg,
                         socket_to_world, socket_to_amz)
     print("dock amz ends")
-    
 
 
 def dock_world(socket_to_world, socket_to_amz):
@@ -104,6 +99,22 @@ def dock_world(socket_to_world, socket_to_amz):
                         socket_to_world, socket_to_amz)
     print("dock world ends")
 
+
+def dock_frontend(socket_to_world, socket_to_amz):
+    try:
+        print("dock to frontend")
+        ip_port_frontend = ('127.0.0.1', 8888)
+        s_to_frontend = socket.socket()
+        s_to_frontend.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s_to_frontend.bind(ip_port_frontend)
+        s_to_frontend.listen(5)
+        while True:
+            frontend, _ = s_to_frontend.accept()
+            t = Thread(target=handle_frontend, args=(frontend, socket_to_world, socket_to_amz))
+            t.setDaemon(True)
+            t.start()
+    except Exception as ex:
+        print(ex)
 
 """
 Helper function for writing, write msg to socket_
@@ -122,7 +133,7 @@ def recv_msg(socket_) -> str:
     var_int_buff = []
     while True:
         buf = socket_.recv(1)
-        if len(buf) <= 0: # socket closes
+        if len(buf) <= 0:  # socket closes
             return None, True
         var_int_buff += buf
         print(str(var_int_buff))
@@ -195,6 +206,7 @@ def recv_from_amz(socket_to_amazom) -> UA.AUmessage:
     except:
         print("Error parsing message from amz\n")
         return None, False
+
 
 def connect_to_word(truck_num, socket_to_world) -> bool:
     msg = World_UPS.UConnect()
@@ -320,14 +332,14 @@ def handle_world_finished(u_finished, socket_to_world, socket_to_amz):
             # update truck & package status to loading
             truck.status = "LOADING"
             truck.save()
-            packages = md.Package.objects.filter(truckid=truck.truckid).filter(status="in WH")
+            packages = md.Package.objects.filter(
+                truckid=truck.truckid).filter(status="in WH")
             if packages:
                 for package in packages:
                     package.status = "loading"
-                    package.save()  
+                    package.save()
         except Exception as ex:
             print(ex)
-    
 
 
 def handle_world_delievered(u_delivery_made, socket_to_world, socket_to_amz):
@@ -348,7 +360,7 @@ def handle_world_delievered(u_delivery_made, socket_to_world, socket_to_amz):
         print(str(package) + " saved")
 
         # update truck's pac_num
-        truck = md.Truck.objects.filter(truckid = truck_id).first()
+        truck = md.Truck.objects.filter(truckid=truck_id).first()
         print("352 to decrease pac_num: " + str(truck))
         truck.pac_num -= 1
         truck.save()
@@ -367,8 +379,10 @@ def handle_world_delievered(u_delivery_made, socket_to_world, socket_to_amz):
 def is_acked(our_req_seq_num) -> bool:
     return md.AckedCommand.objects.filter(seqnum=our_req_seq_num).exists()
 
+
 def res_handled(world_res_seq_num) -> bool:
     return md.WorldRes.objects.filter(seqnum=world_res_seq_num).exists()
+
 
 def handle_world_truck_status(truck_status, socket_to_world, socket_to_amz):
     # save hanlded response from world
@@ -411,10 +425,11 @@ def handle_world(u_rsp: World_UPS.UResponses, socket_to_world, socket_to_amz):
         acked_command.seqnum = ack_
         acked_command.save()
         print("saved\n")
-    
+
     for truck_status in u_rsp.truckstatus:
         if not res_handled(truck_status.seqnum):
-            handle_world_truck_status(truck_status, socket_to_world, socket_to_amz)
+            handle_world_truck_status(
+                truck_status, socket_to_world, socket_to_amz)
 
     for err_ in u_rsp.error:
         print(err_)
@@ -429,8 +444,9 @@ def pick_truck(wh_id):
     lock.acquire()
     while True:
         try:
-        # check whether already exist truck on the way to the WH
-            assigned_truck = md.AssignedTruck.objects.filter(whid=wh_id).first()
+            # check whether already exist truck on the way to the WH
+            assigned_truck = md.AssignedTruck.objects.filter(
+                whid=wh_id).first()
             if assigned_truck:
                 return assigned_truck.truckid.truckid
             # sort the result sorted from IDLE to DELIVERING, pick the 1st one
@@ -438,10 +454,10 @@ def pick_truck(wh_id):
                 status='DELIVERING')).order_by('-status').first()
             print(truck)
             if truck:
-                print("the picked truck is:" + str(truck))
-                 # update truck status to Traveling
-                print("truck to update: " + str(truck))
-                truck.status='TRAVELING'
+                print("445 the picked truck is:" + str(truck))
+                # update truck status to Traveling
+                print("447 truck to update: " + str(truck))
+                truck.status = 'TRAVELING'
                 truck.save()
                 print(str(truck) + " save successfully")
                 return truck.truckid
@@ -484,7 +500,8 @@ def w_a_pickup(truck_id, wh_id, pickup, socket_to_world, socket_to_amz):
     print("461 constructing pickup ins to world...")
     try:
         # insert into DB: AssignedTrucks
-        truck, not_assigned = md.AssignedTruck.objects.get_or_create(whid=wh_id, truckid=md.Truck.objects.filter(truckid=truck_id).first())
+        truck, not_assigned = md.AssignedTruck.objects.get_or_create(
+            whid=wh_id, truckid=md.Truck.objects.filter(truckid=truck_id).first())
         # global seqnum atomically increase seqnum += 1
         seqnum_ = get_seqnum()
         go_pickup = None
@@ -492,14 +509,14 @@ def w_a_pickup(truck_id, wh_id, pickup, socket_to_world, socket_to_amz):
         if not_assigned:
             go_pickup = PBwrapper.go_pickup(truck_id, wh_id, seqnum_)
             print("468 send go_pickup = " + str(go_pickup))
-        # send pickup response to amazon 
+        # send pickup response to amazon
         a_pickup(truck_id, pickup, socket_to_amz)
         # send UCommands(UGoPickup) to World
         if not_assigned:
+            u_commands = World_UPS.UCommands()
+            u_commands.pickups.append(go_pickup)
+            print("518 send to world: " + str(u_commands))
             while not is_acked(seqnum_):
-                u_commands = World_UPS.UCommands()
-                u_commands.pickups.append(go_pickup)
-                print("473 send to world: " + str(u_commands))
                 write_to_world(socket_to_world, u_commands)
                 time.sleep(1)
     except Exception as ex:
@@ -518,11 +535,11 @@ def a_pickup(truck_id, pickup: UA.APacPickup, socket_to_amz):
         print("500 inserting package " + str(ship_id))
         truck = md.Truck.objects.filter(truckid=truck_id).first()
         package = md.Package()
-        package.shipment_id=ship_id
-        package.truckid=truck
-        package.x=pickup.x
-        package.y=pickup.y
-        package.status='in WH'
+        package.shipment_id = ship_id
+        package.truckid = truck
+        package.x = pickup.x
+        package.y = pickup.y
+        package.status = 'in WH'
         package.save()
         print()
         # update package with username if valid
@@ -587,9 +604,12 @@ def handle_amz_all_loaded(all_loaded: UA.ASendAllLoaded, socket_to_world, socket
             for item in package.items:
                 print("563 process item: " + str(item))
                 # insert into fProduct
-                item_ = md.Item.objects.create(id=item.product_id, description=item.description, count=item.count, tracking_id=track)
+                item_ = md.Item.objects.create(
+                    id=item.product_id, description=item.description, count=item.count, tracking_id=track)
                 print("581 inserted item: " + str(item))
-            pac_list.append(PBwrapper.gene_package(ship_id, package.x, package.y))
+            # read pack's dest from db
+            pac_list.append(PBwrapper.gene_package(
+                ship_id, track.x, track.y))
             # track is_a package
             track.status = "loaded"
             track.save()
@@ -601,13 +621,14 @@ def handle_amz_all_loaded(all_loaded: UA.ASendAllLoaded, socket_to_world, socket
         truck.pac_num += len(pac_list)
         truck.save()
         print("603 " + str(truck) + " saved")
-        # global seqnum atomically increase 
+        # global seqnum atomically increase
         # send Ucommands(UGoDeliver) to World
         seqnum_ = get_seqnum()
-        go_deliver = PBwrapper.go_deliver(all_loaded.truck_id, pac_list, seqnum_)
+        go_deliver = PBwrapper.go_deliver(
+            all_loaded.truck_id, pac_list, seqnum_)
+        u_commands = World_UPS.UCommands()
+        u_commands.deliveries.append(go_deliver)
         while not is_acked(seqnum_):
-            u_commands = World_UPS.UCommands()
-            u_commands.deliveries.append(go_deliver)
             write_to_world(socket_to_world, u_commands)
             time.sleep(1)
         # change truck & packages status to delivering after world handeled UGoDeliver
@@ -678,6 +699,26 @@ def a_worldid(socket_to_amz, worldid):
     return
 
 
+def handle_frontend(frontend, socket_to_world, socket_to_amz):
+    try:
+        while True:
+            data = frontend.recv(1024)
+            if data <= 0:
+                frontend.close()
+                return
+            data = data.decode()
+            info = data.split(",")
+            print("710 recv from frontend: " + str(info)) # truckid,packageid,x,y
+            pack = md.Package.objects.get(int(info[1]))
+            if pack.status == "delivering" or pack.status == "dilivered":
+                return
+            pack.x = int(info[2])
+            pack.y = int(info[3])
+            pack.save()
+            
+    except Exception as ex:
+        print(ex)
+
 def main():
     if len(sys.argv) < 3:
         print(
@@ -722,18 +763,23 @@ def main():
     if not retry:
         print("Failed to conenct the world" + "\n")
         sys.exit()
-    
+
     socket_to_amz = get_socket_to_amz()
     # send the world_id to amz
     a_worldid(socket_to_amz, world_id)
-    # start one thread to dock amz
     try:
-        t_to_amz = Thread(target=dock_amz, args=(socket_to_world, socket_to_amz))
+        # start one thread to dock amz
+        t_to_amz = Thread(target=dock_amz, args=(
+            socket_to_world, socket_to_amz))
 
         # start one thread to dock world
         t_to_world = Thread(target=dock_world, args=(
             socket_to_world, socket_to_amz))
 
+        t_to_frontend = Thread(target=dock_frontend, args=(
+            socket_to_world, socket_to_amz), daemon=True)
+        print("Is t_to_front set as Daemon? " + t_to_frontend.isDaemon())
+        t_to_frontend.start()
         t_to_world.start()
         t_to_amz.start()
 
