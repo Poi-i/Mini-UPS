9d99edf3e81e57dc15ba21acccedb78c0bd22510
bw224 <bw224@duke.edu>
2022-04-23 16:26:27 -0400

    item detail, bind package, change dest

--- miniUPS/website/forms.py
+++ miniUPS/website/forms.py
@@ -17,6 +17,7 @@ class RegisterForm(forms.Form):
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username', required=True, widget=forms.TextInput(
         attrs={'class': 'form-control form-control-lg', 'placeholder': 'Enter your user name'}))
+
     password = forms.CharField(label='Password', required=True, widget=forms.PasswordInput(
         attrs={'class': 'form-control form-control-lg', 'placeholder': 'Enter password'}))
 
@@ -33,5 +34,8 @@ class TrackForm(forms.Form):
     #             "The tracking number you entered is not valid.")
 
 
-# class PackageForm(forms.Form):
-#     package_user = forms.CharField(abel='bind username', required=True)
+class DestAddrForm(forms.Form):
+    x = forms.IntegerField(label='x', required=True, widget=forms.NumberInput(
+        attrs={'class': 'form-control', 'placeholder': 'x'}))
+    y = forms.IntegerField(label='y', required=True, widget=forms.NumberInput(
+        attrs={'class': 'form-control', 'placeholder': 'y'}))
